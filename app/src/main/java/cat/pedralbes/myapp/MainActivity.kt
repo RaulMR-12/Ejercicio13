6eb887244bf7f1559fee8ddacec2be10f76919d1
RaulMR-12 <rmartinezromero12@gmail.com>
2024-01-18 17:21:26 +0100

    Bugfix

--- app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
+++ app/src/main/java/cat/pedralbes/myapp/MainActivity.kt
@@ -2,6 +2,7 @@ package cat.pedralbes.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio13.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
